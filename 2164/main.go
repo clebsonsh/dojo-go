3d0fa357502a2168b72fcd3d571794a9aec8e6e9
Clebson Moura <clebson.sh@gmail.com>
2024-10-04 14:10:33 -0300

    2164 solution

--- 2164/main.go
+++ 2164/main.go
@@ -1,6 +1,15 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 // https://judge.beecrowd.com/pt/problems/view/2164
 func main() {
+	var n float64 = 0
+	fmt.Scanln(&n)
 
+	var output float64 = ((math.Pow((1+math.Sqrt(5))/2, n)) - (math.Pow((1-math.Sqrt(5))/2, n))) / math.Sqrt(5)
+	fmt.Printf("%.1f\n", output)
 }
